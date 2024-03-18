3f6c11b3a85209574b623425139df8a071cc6496
Rin-t <rin.t.4921@gmail.com>
2024-03-18 23:39:14 +0900

    Initial Commit

---
+++ CounterFeatureSample/CounterFeatureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterFeatureSampleApp.swift
+//  CounterFeatureSample
+//
+//  Created by Rin on 2024/03/18.
+//
+
+import SwiftUI
+
+@main
+struct CounterFeatureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
