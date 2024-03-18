2fb2147540bfeb80fc808e461fce2c9b34b5763f
Rin-t <rin.t.4921@gmail.com>
2024-03-19 00:13:33 +0900

    Viewの作成

--- CounterFeatureSample/ContentView.swift
+++ CounterFeatureSample/ContentView.swift
@@ -6,19 +6,51 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct ContentView: View {
+
+    let store: StoreOf<CounterFeature>
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(store.count)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .presentationCornerRadius(10)
+
+            HStack {
+                Button("-") {
+                    store.send(.decrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .presentationCornerRadius(10)
+
+                Button("+") {
+                    store.send(.incrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .presentationCornerRadius(10)
+            }
+            Button("Fact") {
+                store.send(.factButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .presentationCornerRadius(10)
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+    }
+    return ContentView(store: store)
 }
