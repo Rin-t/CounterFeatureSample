d10f1c4e6de4bccd1021118f3cc89fa5b3c8ee1f
Rin-t <rin.t.4921@gmail.com>
2024-03-19 00:15:27 +0900

    Storeを引数に渡すように修正

--- CounterFeatureSample/CounterFeatureSampleApp.swift
+++ CounterFeatureSample/CounterFeatureSampleApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct CounterFeatureSampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            let store = Store(initialState: CounterFeature.State()) {
+                CounterFeature()
+            }
+            ContentView(store: store)
         }
     }
 }
